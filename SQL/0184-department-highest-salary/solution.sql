13e49cdd5a549ffc15742df3af4579d1a3981e2e
Ankith Reddy Vemula <arv4@sfu.ca>
2026-04-16 06:40:47 +0000

    Sync LeetCode submission Runtime - 651 ms (57.67%), Memory - 0.0B (100.00%)

--- SQL/0184-department-highest-salary/solution.sql
+++ SQL/0184-department-highest-salary/solution.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-WITH SalRank as (
-    SELECT d.name as Department, e.name as Employee, 
-    e.salary as Salary,
-    RANK() OVER(partition by d.id ORDER BY e.salary DESC) as ran
-    FROM employee e
-    JOIN department d ON d.id = e.departmentId
+WITH DeptHighestSal AS (
+    SELECT d.name as Department, e.name as Employee, e.salary as Salary,
+    DENSE_RANK() OVER(partition by d.id ORDER BY e.salary DESC) as ranks
+    FROM Employee e
+    JOIN Department d ON d.id = e.departmentId
 )
 
 SELECT Department, Employee, Salary
-FROM SalRank
-WHERE ran = 1
+FROM DeptHighestSal
+WHERE ranks = 1
